465bdf54ee1da778d4081a3d110750f70de4eb70
sofeae <102640907+sofeae@users.noreply.github.com>
2022-03-30 11:39:43 +0800

    Add files via upload

---
+++ student.java
@@ -0,0 +1,36 @@
+package assignment1;
+
+import java.sql.*; 
+
+public class student {
+
+	public static void main(String[] args) throws SQLException { 
+		// Connect to database
+Connection conn = null;
+conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/db1?" + "user=root&password=sofeaezzaty@25");	
+	
+		// Create a statement to retrieve objects
+		Statement s = conn.createStatement();        
+
+		// Retrieve results from the table
+		ResultSet rs = s.executeQuery ("Select * from Student"); 
+		
+		// Print columns headers
+		ResultSetMetaData rsmd =rs.getMetaData(); 
+		int i =rsmd.getColumnCount( );
+		
+		for(int j=1; j<=i; j++) { 
+			System.out.print(rsmd.getColumnName(j)+"\t"); 
+			System.out.print(" "); 
+		} 
+
+		// Print records
+		while(rs.next( ) ) { 
+			for(int j=1; j<=i; j++) 
+			{ 
+				System.out.print ( rs.getString( j)+"\t"); 
+			}
+		}
+		
+	}
+}
